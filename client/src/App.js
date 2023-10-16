bdf2d97288c80525988188fd13c091ef92f48758
duskcoder <duskcodertec@gmail.com>
2023-10-16 12:19:00 +0530

    venu pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { Header } from './Component/Header';
+import { AboutUs } from './Component/AboutUs';
+// import  AboutUs from "./Component/AboutUs"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      
+    <div >
+    <BrowserRouter>
+    <Routes>
+      <Route path='/about' element={<AboutUs/>}/>
+    </Routes>
+    
+    </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/Component/AboutUs.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const AboutUs = () => {
+  return (
+    <>
+    <div  id='aboutUs'>
+        <div className='container'>
+    <div class="card col-lg-6" >
+  <div class="card-body">
+    <h5 class="card-title">About Us</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+  </div>
+</div>
+
+</div>  
+    </div>
+    </>
+  )
+}
