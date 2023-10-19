62bae28d8522788dd4fee056f4ebd4ebf4dbdfcd
duskcoder <duskcodertec@gmail.com>
2023-10-19 16:24:50 +0530

    venu pull

--- client/src/Component/Header.js
+++ client/src/Component/Header.js
@@ -7,14 +7,15 @@ const Header = () => {
     <div className='hello'>
 
       <nav class="navbar navbar-expand-lg ">
-        <div class="container-fluid ">
-          <div className='col-lg-2'></div>
+        <div class="container">
+          {/* <div className='col-lg-2'></div> */}
           <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <div className='col-lg-1'></div>
+          
+          <div class="collapse navbar-collapse " id="navbarSupportedContent">
+        
             <ul class="navbar-nav me-auto ">
               <li class="nav-item">
                 <Link to='/' class="nav-link move" aria-current="page">Home</Link>
@@ -39,7 +40,7 @@ const Header = () => {
 
             <button class="btn btn-head" type="submit">BlueMoon HomeStay</button>
           </form>
-          <div className='col-lg-2'></div>
+          {/* <div className='col-lg-2'></div> */}
         </div>
       </nav>
     </div>
