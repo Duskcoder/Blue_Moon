54d83df2b8d904951adc9b40400a344994912176
duskcoder <duskcodertec@gmail.com>
2023-12-14 20:00:07 +0530

    gitpull

--- client/src/Component/AboutUs1.js
+++ client/src/Component/AboutUs1.js
@@ -5,7 +5,7 @@ export const AboutUs1 = () => {
   return (
     <>
       <div id="aboutUs1" className="overflow-hidden">
-        <div className="container mt-5">
+        <div className="abouta mt-5">
           <div className="row">
             <div id="aboutwhite"
               className="card  hello2  mt-5  col-12"
@@ -72,75 +72,79 @@ export const AboutUs1 = () => {
           </div>
         </div>
 
-        <div className=" pt-5">
+    <div className="container  ">
+      <div className="row">
+      <div className="aboutb pt-5 col-12 ">
          
-            <div id="aboutwhite "
-              className="card  hello2  mt-5  "
-              data-aos="fade-right"
-              data-aos-duration="1500"
-            >
-              <div className="">
-                <div className="card-body">
-                  <div className="hello22 mt-5" data-aos-duration="1000"></div>
-                  <h2
-                    className="card-title text-center mt-5"
-                    data-aos="fade-right"
-                    data-aos-duration="500"
-                    style={{ color: "#2a919d" }}
-                  >
-                    {" "}
-                    About Us
-                  </h2>
-                  <h3 className="card-subtitle mt-4 text-center">
-                    Experience A Stay
-                    <br /> Like Never Before
-                  </h3>
-                  <div
-                    className="mt-5 text-center"
-                    data-aos="fade-right"
-                    data-aos-duration="500"
-                  >
-                    <p className="card-text">
-                      We will help your experience life in its true sense{" "}
-                    </p>
-                    <p>With us. from living among the residents you are </p>
-                    <p> sure to walk home with memories and wonderful </p>
-                    <p> moments.</p>
-                  </div>
-                  <div
-                    className="hello22 mt-5 mb-5 w"
-                    data-aos="fade-right"
-                    data-aos-duration="1000"
-                  ></div>
-                </div>
-              </div>
-            </div>
+         <div id="aboutwhite "
+           className="card  hello2  mt-5  "
+           data-aos="fade-right"
+           data-aos-duration="1500"
+         >
+           <div className="">
+             <div className="card-body">
+               <div className="hello22 mt-5" data-aos-duration="1000"></div>
+               <h2
+                 className="card-title text-center mt-5"
+                 data-aos="fade-right"
+                 data-aos-duration="500"
+                 style={{ color: "#2a919d" }}
+               >
+                 {" "}
+                 About Us
+               </h2>
+               <h3 className="card-subtitle mt-4 text-center">
+                 Experience A Stay
+                 <br /> Like Never Before
+               </h3>
+               <div
+                 className="mt-5 text-center"
+                 data-aos="fade-right"
+                 data-aos-duration="500"
+               >
+                 <p className="card-text">
+                   We will help your experience life in its true sense{" "}
+                 </p>
+                 <p>With us. from living among the residents you are </p>
+                 <p> sure to walk home with memories and wonderful </p>
+                 <p> moments.</p>
+               </div>
+               <div
+                 className="hello22 mt-5 mb-5 "
+                 data-aos="fade-right"
+                 data-aos-duration="1000"
+               ></div>
+             </div>
+           </div>
+         </div>
 
 
-       
-          <div
-            className="card hello1    "
-            data-aos="fade-left"
-            data-aos-duration="1500"
-          >
-            <div
-              className="card-body card-bodys mt-5 mb-5 text-center"
-              data-aos="fade-left"
-              data-aos-duration="1000"
-            >
-              <p className="card-text1 mt-5">
-                We are a well renowned, providing a sensational
-              </p>
-              <p>staying experience eith prolific services. We give </p>
-              <p>you the warmest of welcome and all the personal</p>
-              <p>attention at our homestay to ensure your</p>
-              <p>comfort and contentment. We are the first choice </p>
-              <p>option for the one who are looking to explore </p>
-              <p>the local areas and beautiful places in</p>
-              <p>Puducherry.</p>
-            </div>
-          </div>
-        </div>
+    
+       <div
+         className="card hello1    "
+         data-aos="fade-left"
+         data-aos-duration="1500"
+       >
+         <div
+           className="card-body card-bodys mt-5 mb-5 text-center"
+           data-aos="fade-left"
+           data-aos-duration="1000"
+         >
+           <p className="card-text1 mt-5">
+             We are a well renowned, providing a sensational
+           </p>
+           <p>staying experience eith prolific services. We give </p>
+           <p>you the warmest of welcome and all the personal</p>
+           <p>attention at our homestay to ensure your</p>
+           <p>comfort and contentment. We are the first choice </p>
+           <p>option for the one who are looking to explore </p>
+           <p>the local areas and beautiful places in</p>
+           <p>Puducherry.</p>
+         </div>
+       </div>
+     </div>
+      </div>
+    </div>
 
       </div>
     </>
---
+++ client/src/Component/Cancellation/Cancellation.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import "../../Component/Rooms/Rooms.css";
+import Header1 from '../Header1'
+import Footer1 from '../HomeStay/Footer1'
+function Cancellation() {
+    return (
+        <>
+            <Header1 />
+            <div className="room col-6 col-lg-12 htags pt-5 mx-5 overflow-hidden">
+
+                <h1 className style={{ color: ' #2a919d' }} data-aos='fade-left' data-aos-duration='500'>Rooms</h1>
+
+                <div className='mt-4 ' style={{ color: ' #2a919d' }} data-aos='fade-right' data-aos-duration='500'>
+                    <p className="ptag">
+                        If you are looking for a home away from home, just walk in
+                        here.
+                        <br /> You can get both, an incredible and relaxing experience
+                        while you are on a holiday
+                    </p>   </div>
+            </div>
+            <div className="room col-6 col-lg-12 htagsMob mx-5">
+                <h1 className=" htagr" style={{ color: ' #2a919d' }} data-aos='fade-left' data-aos-duration='500'>Rooms</h1>
+                <p className="ptag "id='cancelp' style={{ color: ' #2a919d' }} data-aos='fade-left' data-aos-duration='500'>
+                    If you are looking for a home away from home, just walk in
+                    here.
+                    <br /> You can get both, an incredible and relaxing experience
+                    while you are on a holiday
+                </p>
+            </div>
+
+            <Footer1 />
+
+
+        </>
+    )
+}
+
+export default Cancellation
